a7ae766765b122fe745d70386a3d024b35bfbdbe
Estefy Rabih <tefyrabih@gmail.com>
2021-10-19 13:12:50 -0500

    creación de funcion de estadisticas -- stats

---
+++ stats.js
@@ -0,0 +1,18 @@
+ const calculateStats = (linkObject) => {
+    let arrayLinks = []
+    linkObject.forEach(e =>arrayLinks.push(e.href));
+    const totalLinks = arrayLinks.length;
+    const uniqueLinks = new Set(arrayLinks);
+    const unique = [...uniqueLinks].length;
+    let brokenLinks = 0;
+    linkObject.map((e)=> {
+     if (e.statusText === 'FAIL')
+     brokenLinks++
+    })
+    return {
+      totalLinks,
+      brokenLinks,
+      unique,
+    }
+  }
+  module.exports = calculateStats
